fbbfe7a070e991d98b9abc344e4cebdfafcff46e
chunza2542 <chunza2542@hotmail.com>
2018-10-24 20:39:54 +0700

    sorted by committee score

--- src/admin/Candidates.js
+++ src/admin/Candidates.js
@@ -15,103 +15,6 @@ const Padding = styled.div`
   padding-bottom: 5px;
 `;
 
-const columns = [
-  {
-    title: "Name",
-    key: "name",
-    render: user => (
-      <span>
-        {user.firstNameEN} {user.lastNameEN} ({user.nickname})
-      </span>
-    ),
-    fixed: "left",
-  },
-  {
-    title: "Status",
-    key: "status",
-    dataIndex: "status",
-    render: status => (
-      <span>
-        <Tag color={status === "completed" ? "green" : "volcano"}>{status}</Tag>
-      </span>
-    ),
-  },
-  {
-    title: "Major",
-    dataIndex: "major",
-    key: "major",
-  },
-  {
-    title: "Email",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "Phone Number",
-    dataIndex: "phone",
-    key: "phone",
-  },
-  {
-    title: "Sex",
-    dataIndex: "sex",
-    key: "sex",
-  },
-  {
-    title: "Birthdate",
-    dataIndex: "birthdate",
-    key: "birthdate",
-    render: date => (
-      <span>
-        {moment(new Date(date)).format("MM/DD/YYYY")} (Age:{" "}
-        {moment().diff(new Date(date), "years")})
-      </span>
-    ),
-  },
-  {
-    title: "YWC Staff Check Status",
-    dataIndex: "isPassStaff",
-    key: "isPassStaff",
-    render: status => (
-      <span>
-        <Tag color={status === true ? "green" : "geekblue"}>
-          {status === true ? "Checked" : "Don't Check"}
-        </Tag>
-      </span>
-    ),
-  },
-  {
-    title: "Grading Status",
-    dataIndex: "failed",
-    key: "failed",
-    render: failed => (
-      <span>
-        <Tag color={failed === true ? "red" : "purple"}>
-          {failed === true ? "Failed" : "In System"}
-        </Tag>
-      </span>
-    ),
-  },
-  {
-    title: "Committee Score",
-    dataIndex: "committeeScore",
-    key: "committeeScore",
-    render: score => <span>{score || 0}</span>,
-  },
-  {
-    title: "Action",
-    key: "action",
-    render: data => (
-      <Button>
-        <Link params={{id: data.id}} to={`/candidates/${data._id}`}>
-          <Icon type="edit" theme="outlined" /> ดูรายละเอียเพิ่มเติม
-        </Link>
-      </Button>
-    ),
-    width: 200,
-    fixed: "right",
-  },
-];
-
 const mapStateToProps = state => ({
   auth: state.auth,
 });
@@ -128,6 +31,13 @@ export default class Candidates extends Component {
     this.getCandidates();
   };
 
+  handleChange = (_, filters, sorter) => {
+    this.setState({
+      filteredInfo: filters,
+      sortedInfo: sorter,
+    });
+  };
+
   getCandidates = async () => {
     const {major} = this.props.auth.profile;
     const response = await fetchWithToken("users/all", {major}, "GET");
@@ -137,13 +47,113 @@ export default class Candidates extends Component {
     }
   };
 
+  columns = [
+    {
+      title: "Name",
+      key: "name",
+      render: user => (
+        <span>
+          {user.firstNameEN} {user.lastNameEN} ({user.nickname})
+        </span>
+      ),
+      fixed: "left",
+    },
+    {
+      title: "Status",
+      key: "status",
+      dataIndex: "status",
+      render: status => (
+        <span>
+          <Tag color={status === "completed" ? "green" : "volcano"}>
+            {status}
+          </Tag>
+        </span>
+      ),
+    },
+    {
+      title: "Major",
+      dataIndex: "major",
+      key: "major",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Phone Number",
+      dataIndex: "phone",
+      key: "phone",
+    },
+    {
+      title: "Sex",
+      dataIndex: "sex",
+      key: "sex",
+    },
+    {
+      title: "Birthdate",
+      dataIndex: "birthdate",
+      key: "birthdate",
+      render: date => (
+        <span>
+          {moment(new Date(date)).format("MM/DD/YYYY")} (Age:{" "}
+          {moment().diff(new Date(date), "years")})
+        </span>
+      ),
+    },
+    {
+      title: "YWC Staff Check Status",
+      dataIndex: "isPassStaff",
+      key: "isPassStaff",
+      render: status => (
+        <span>
+          <Tag color={status === true ? "green" : "geekblue"}>
+            {status === true ? "Checked" : "Don't Check"}
+          </Tag>
+        </span>
+      ),
+    },
+    {
+      title: "Grading Status",
+      dataIndex: "failed",
+      key: "failed",
+      render: failed => (
+        <span>
+          <Tag color={failed === true ? "red" : "purple"}>
+            {failed === true ? "Failed" : "In System"}
+          </Tag>
+        </span>
+      ),
+    },
+    {
+      title: "Committee Score",
+      dataIndex: "committeeScore",
+      key: "committeeScore",
+      render: score => <span>{score || 0}</span>,
+      sorter: (a, b) => (a.committeeScore || 0) - (b.committeeScore || 0),
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: data => (
+        <Button>
+          <Link params={{id: data.id}} to={`/candidates/${data._id}`}>
+            <Icon type="edit" theme="outlined" /> ดูรายละเอียเพิ่มเติม
+          </Link>
+        </Button>
+      ),
+      width: 200,
+      fixed: "right",
+    },
+  ];
+
   render() {
     return (
       <Fragment>
         <Padding>Hello world</Padding>
 
         <Table
-          columns={columns}
+          columns={this.columns}
           scroll={{x: 2000}}
           dataSource={this.candidates}
         />
