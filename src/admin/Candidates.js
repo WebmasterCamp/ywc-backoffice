23d218b93765232fbd909165021ab877c0be7d1c
chunza2542 <chunza2542@hotmail.com>
2018-10-24 20:24:07 +0700

    render users on admin/candidates

--- src/admin/Candidates.js
+++ src/admin/Candidates.js
@@ -7,12 +7,7 @@ import {Table, Icon, Button, Tag} from "antd";
 import {Link} from "react-router-dom";
 
 import {authen} from "../utils/authen";
-import {fetch, fetchWithToken} from "../utils/fetch";
-
-const Stat = styled.div`
-  color: #777;
-  margin-bottom: 20px;
-`;
+import {fetchWithToken} from "../utils/fetch";
 
 const Padding = styled.div`
   padding: 20px;
@@ -21,14 +16,14 @@ const Padding = styled.div`
 
 const columns = [
   {
-    title: "No.",
-    dataIndex: "key",
-    key: "key",
-  },
-  {
-    title: "User ID.",
-    dataIndex: "id",
-    key: "id",
+    title: "Name",
+    key: "name",
+    render: user => (
+      <span>
+        {user.firstNameEN} {user.lastNameEN} ({user.nickname})
+      </span>
+    ),
+    fixed: "left",
   },
   {
     title: "Status",
@@ -36,22 +31,77 @@ const columns = [
     dataIndex: "status",
     render: status => (
       <span>
-        <Tag color={!status ? "red" : "green"}>
-          {!status ? "ยังไม่ได้ตรวจ" : "ตรวจแล้ว"}
+        <Tag color={status === "completed" ? "green" : "volcano"}>{status}</Tag>
+      </span>
+    ),
+  },
+  {
+    title: "Major",
+    dataIndex: "major",
+    key: "major",
+  },
+  {
+    title: "Email",
+    dataIndex: "email",
+    key: "email",
+  },
+  {
+    title: "Phone Number",
+    dataIndex: "phone",
+    key: "phone",
+  },
+  {
+    title: "Sex",
+    dataIndex: "sex",
+    key: "sex",
+  },
+  {
+    title: "Birthdate",
+    dataIndex: "birthdate",
+    key: "birthdate",
+  },
+  {
+    title: "YWC Staff Check Status",
+    dataIndex: "isPassStaff",
+    key: "isPassStaff",
+    render: status => (
+      <span>
+        <Tag color={status === true ? "green" : "geekblue"}>
+          {status === true ? "Checked" : "Don't Check"}
         </Tag>
       </span>
     ),
+  },
+  {
+    title: "Grading Status",
+    dataIndex: "failed",
+    key: "failed",
+    render: failed => (
+      <span>
+        <Tag color={failed === true ? "red" : "purple"}>
+          {failed === true ? "Failed" : "In System"}
+        </Tag>
+      </span>
+    ),
+  },
+  {
+    title: "Committee Score",
+    dataIndex: "committeeScore",
+    key: "committeeScore",
+    render: score => <span>{score || 0}</span>,
   },
   {
     title: "Action",
     key: "action",
     render: data => (
       <Button>
-        <Link params={{id: data.id}} to={`/committee/${data.id}`}>
-          <Icon type="edit" theme="outlined" /> ตรวจคำถาม
+        <Link params={{id: data.id}} to={`/candidates/${data._id}`}>
+          <Icon type="edit" theme="outlined" /> ดูรายละเอียเพิ่มเติม
         </Link>
       </Button>
     ),
+    width: 200,
+    fixed: "right",
   },
 ];
 
@@ -64,66 +114,31 @@ const mapStateToProps = state => ({
 @observer
 export default class Candidates extends Component {
   @observable
-  totalCandidates = 0;
-  @observable
   candidates = [];
-  @observable
-  passStaff = 0;
 
   // fetch and render data
   componentDidMount = async () => {
     this.getCandidates();
-    this.getStat();
   };
 
   getCandidates = async () => {
     const {major} = this.props.auth.profile;
-    const response = await fetchWithToken("users/committee", {major}, "GET");
+    const response = await fetchWithToken("users/all", {major}, "GET");
 
     if (response.status === "success") {
       this.candidates = response.payload;
     }
   };
 
-  getStat = async () => {
-    const userResponse = await fetch("users/stat");
-    const users = userResponse.payload;
-
-    const statResponse = await fetchWithToken(
-      "users/committee/stat",
-      {},
-      "GET",
-    );
-
-    this.passStaff = statResponse.payload.passStaff;
-    this.totalCandidates = users[this.props.auth.profile.major];
-  };
-
   render() {
-    const {profile} = this.props.auth;
-
     return (
       <Fragment>
-        <Padding>
-          <Stat>
-            จำนวนใบสมัครทั้งหมด {this.totalCandidates} จำนวนที่ผ่านการคัด{" "}
-            {this.passStaff} จำนวนที่ตรวจแล้ว{" "}
-            {
-              this.candidates.filter(
-                x => x.committeeVote.indexOf(profile.username) !== -1,
-              ).length
-            }{" "}
-            ({profile.major})
-          </Stat>
-        </Padding>
+        <Padding>Hello world</Padding>
 
         <Table
           columns={columns}
-          dataSource={this.candidates.map((candidate, i) => ({
-            key: i + 1,
-            id: candidate._id,
-            status: candidate.committeeVote.indexOf(profile.username) !== -1,
-          }))}
+          scroll={{x: 2000}}
+          dataSource={this.candidates}
         />
       </Fragment>
     );
