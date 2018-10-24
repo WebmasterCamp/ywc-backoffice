bfcd6768a3835ac755f4501f50d320200ac23b37
chunza2542 <chunza2542@hotmail.com>
2018-10-24 19:09:09 +0700

    add route /admin/candidates

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,10 +1,11 @@
 import React, {Component, Fragment} from "react";
 
+import Candidates from "./Candidates";
 import Dashboard from "../dashboard";
 import Menubar from "../common/Menubar";
 
 export default class Admin extends Component {
-  getUserIdentity = () => {
+  getRoute = () => {
     const url = window.location.href.split("/");
     return url[url.length - 1];
   };
@@ -14,8 +15,12 @@ export default class Admin extends Component {
       <Fragment>
         <Menubar
           header={`Dashboard`}
-          menus={[{icon: "user", name: "Dashboard", to: "/admin"}]}>
-          <Dashboard />
+          menus={[
+            {icon: "user", name: "Dashboard", to: "/admin"},
+            {icon: "user", name: "All Candidates", to: "/admin/candidates"},
+          ]}>
+          {this.getRoute() === "candidates" && <Candidates />}
+          {this.getRoute() === "admin" && <Dashboard />}
         </Menubar>
       </Fragment>
     );
