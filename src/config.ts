91d11c8c1e5db6ba97ee1446c4bcccae4a1fed15
Wiput Pootong <wiput.pootong@gmail.com>
2020-08-17 21:10:34 +0700

    update: ywc17

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 const env = process.env.REACT_APP_API_DEV
 
 export const API_ENDPOINT =
-  env === 'true' ? 'https://api-staging.ywc.in.th' : 'https://api.ywc.in.th'
+  env === 'true' ? 'https://api-staging.ywc.in.th' : 'https://api.ywc17.ywc.in.th'
