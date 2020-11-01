846378381be4d93abbcf5c515ba55ef11c01689e
Chayanon Thongpila <nutchy.th@gmail.com>
2020-11-01 08:45:28 +0700

    fix base url

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 const env = process.env.REACT_APP_API_DEV
 
 export const API_ENDPOINT =
-  env === 'true' ? 'http://localhost:5000' : 'https://api-prod.ywc18.ywc.in.th'
+  env === 'true' ? 'https://api-staging.ywc18.ywc.in.th/' : 'https://api-prod.ywc18.ywc.in.th'
